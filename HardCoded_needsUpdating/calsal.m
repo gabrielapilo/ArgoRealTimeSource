20568be996346682e8e43653d252676581632848
Cowley, Rebecca (O&A, Hobart) <rebecca.cowley@csiro.au>
2017-02-14 07:53:06 +1100

    Remove another float from auto salinity calibration

--- HardCoded_needsUpdating/calsal.m
+++ HardCoded_needsUpdating/calsal.m
@@ -152,7 +152,7 @@ for kk = ical(:)'
    %and for 5901707, 5903249 etc because it is in a bad area for calibrating.
 if(fp.wmo_id==1901121 | fp.wmo_id==1901324 | fp.wmo_id==1901321 | ...
         fp.wmo_id==5901707 |fp.wmo_id==5901706 | fp.wmo_id==5903249 | ...
-        fp.wmo_id==5903260 | fp.wmo_id==5901698)
+        fp.wmo_id==5903260 | fp.wmo_id==5901698 | fp.wmo_id==5901693)
     calibrate=0;
     %and turn off calibration for these floats because they are in salty
     %water that's triggering calibration wihtout valid cause:
