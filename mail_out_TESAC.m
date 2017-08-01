d4ced777d6f3f068c3db5f32892bedc5e8426949
Cowley, Rebecca (O&A, Hobart) <rebecca.cowley@csiro.au>
2017-08-01 16:18:33 +1000

    Update TESAC header to include processor.

--- mail_out_TESAC.m
+++ mail_out_TESAC.m
@@ -1,1 +1,1 @@
-[st,ww] = system(['cat ' fnm '| mail -s "[SEC=UNCLASSIFIED] TESAC ' ARGO_SYS_PARAM.inst '" ' ARGO_SYS_PARAM.gts_addrs]);
+[st,ww] = system(['cat ' fnm '| mail -s "[SEC=UNCLASSIFIED] TESAC ' ARGO_SYS_PARAM.processor '" ' ARGO_SYS_PARAM.gts_addrs]);
