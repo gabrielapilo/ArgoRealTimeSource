4c663310961de6b829150a74f4daaeff130af637
Cowley, Rebecca (O&A, Hobart) <rebecca.cowley@csiro.au>
2018-02-14 17:04:35 +1100

    Adjust Argos code to look at maximum of the shallowest depths.
    Also, don't delete location information.

--- process_profile.m
+++ process_profile.m
@@ -370,15 +370,15 @@ if any(stage>=1)
    
    [maxdeps,mindeps] = get_ocean_depth(lats,lons,0.03);
    inan = isnan(mindeps);
-   deps = median(mindeps(~inan));
+   deps = max(mindeps(~inan));
    kk = find(isnan(lats) | isnan(lons) | lats<-90 | lats>90 ...
        | lons<-180 | lons>360 | deps<0);
    if ~isempty(kk)
        logerr(2,'Implausible locations or location on land');
-       gdhed(kk) = [];
-       if isempty(gdhed)
+%        gdhed(kk) = [];
+%        if isempty(gdhed)
            logerr(2,'No good location fixes or location on land!');
-       end
+%        end
    end
 
 
