8c069525e0a71c5fec4b33c8098170970fc19c92
Cowley, Rebecca (O&A, Hobart) <rebecca.cowley@csiro.au>
2018-05-17 12:51:27 +1000

    Uncalibrate salinity bug fix.

--- Utilities_RunByHandCode/uncalibrate_sal.m
+++ Utilities_RunByHandCode/uncalibrate_sal.m
@@ -10,11 +10,12 @@ set_argo_sys_params
 for i=1:length(wmo_id)
     [fpp,dbdat]=getargo(wmo_id(i));
     for j=profrange
-        if fpp(j).c_ratio~=1
-            [fpp,cal_rept]=calsal(fpp,j,0);
-            argoprofile_nc(dbdat,fpp(j));
-            web_profile_plot(fpp(j),dbdat)
-        end
+        %         if fpp(j).c_ratio~=1
+        %re-run the calibration to update the c_ratio values.
+        [fpp,cal_rept]=calsal(fpp,j,0);
+        argoprofile_nc(dbdat,fpp(j));
+        web_profile_plot(fpp(j),dbdat)
+        %         end
     end
     float=fpp;
     
