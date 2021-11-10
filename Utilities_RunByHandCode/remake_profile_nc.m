d6cfe902fbbda54c2d2f17603b1a252ccb2c7a33
Semolini Pilo, Gabriela (O&A, Hobart) <Gabriela.Semolinipilo@csiro.au>
2021-11-11 10:48:28 +1100

    small changed to improve usage

--- Utilities_RunByHandCode/remake_profile_nc.m
+++ Utilities_RunByHandCode/remake_profile_nc.m
@@ -1,6 +1,8 @@
 %recreate netcdf files for selected floats
 %adapt to suit the need!
 
+clear all
+
 global THE_ARGO_FLOAT_DB
 global ARGO_SYS_PARAM
 global THE_ARGO_BIO_CAL_DB  ARGO_BIO_CAL_WMO
@@ -12,73 +14,107 @@ end
 getdbase(-1)
 % PREC_FNM = [ARGO_SYS_PARAM.root_dir 'Argo_proc_records'];
 
-kk = [5905446];
+kk = [7900396 7900397 7900398 7900602 7900603 7900604 7900605 7900606 7900607 7900608];
 ipath = ARGO_SYS_PARAM.iridium_path;
-% for ii = 745:length(THE_ARGO_FLOAT_DB)
-for ii = 1%7:length(kk)
+
+% Loops in floats:
+for ii = 1:length(kk)
+    
     disp(ii)
     [fpp,dbdat]=getargo(kk(ii));
-%     [fpp,dbdat] = getargo(THE_ARGO_FLOAT_DB(ii).wmo_id);
+    
+    %     [fpp,dbdat] = getargo(THE_ARGO_FLOAT_DB(ii).wmo_id);
+    % if any([dbdat.flbb,dbdat.flbb2,dbdat.irr, dbdat.irr2, ...
+    %             dbdat.pH])
 
-% if any([dbdat.flbb,dbdat.flbb2,dbdat.irr, dbdat.irr2, ...
-%             dbdat.pH])
-        %change the path temporarily:
-        ARGO_SYS_PARAM.iridium_path = [ipath 'iridium_processed/' ...
-            num2str(dbdat.wmo_id) '/']; %%% To rebuild from original log and msg files
-%     if dbdat.maker == 4 %4 is Navis
-        for j = 1%23:32%1:length(fpp)+dbdat.np0%71:78%1:
-            clear pmeta
-            close all
-            [ii j]
-%             if ~isempty(fpp(j).lat)
-%                 try
-                    pmeta.wmo_id = dbdat.wmo_id;
-                    pn = '000';
-                    pns = num2str(j);
-                    pn(end-length(pns)+1:end) = pns;
-                    pmeta.ftp_fname = [dbdat.argos_hex_id '.' pn '.msg'];
-                    fn = dirc([ARGO_SYS_PARAM.iridium_path pmeta.ftp_fname]);
-                    pmeta.ftptime = julian(datevec(fn{4}));
-                    
-%                     fn = dirc([ARGO_SYS_PARAM.iridium_path 'f*.' pn '.*science_log.csv']);
-%                     if isempty(fn)
-%                      fn = dirc([ARGO_SYS_PARAM.iridium_path 'f*.' pn '.*system_log.txt']);
-%                     end
-%                     if isempty(fn)
-%                      fn = dirc([ARGO_SYS_PARAM.iridium_path '*.' pn '.*science_log.csv']);
-%                     end
-%                     if isempty(fn)
-%                      fn = dirc([ARGO_SYS_PARAM.iridium_path '*.' pn '.*system_log.txt']);
-%                     end
+    % Path to files
+    ARGO_SYS_PARAM.iridium_path = [ipath 'iridium_processed/' ...
+        num2str(dbdat.wmo_id) '/']; % To rebuild from original, processed log and msg files
+    %
+    % Path to files
+    %         ARGO_SYS_PARAM.iridium_path = [ipath]; % To rebuild from original,non-processed log and msg files, iridium_data
+    
+    % Loops in profiles
+    for j = 1:length(fpp);
+        
+        %           for j = 81%2:10;
+        clear pmeta
+        close all
+        [ii j]
+        
+        %             if ~isempty(fpp(j).lat)
+        %                 try
+        pmeta.wmo_id = dbdat.wmo_id;
+        pn = '000';
+        pns = num2str(j);
+        pn(end-length(pns)+1:end) = pns; % profile number
+        
+        %
+        %                     %% Uncomment for NAVIS and APF9
+        %                     pmeta.ftp_fname = [num2str(dbdat.argos_id) '.' pn '.msg'];
+        pmeta.ftp_fname = [num2str(dbdat.argos_id,'%04.f') '.' pn '.msg'];
+        fn = dirc([ARGO_SYS_PARAM.iridium_path pmeta.ftp_fname]);
+        pmeta.ftptime = julian(datevec(fn{4}));
+        %
+        
+        % % %                     % uncomment for APF11 floats
+        %                     fn = dirc([ARGO_SYS_PARAM.iridium_path 'f*.' pn '.*science_log.csv']);
+        %                     if isempty(fn)
+        %                      fn = dirc([ARGO_SYS_PARAM.iridium_path 'f*.' pn '.*system_log.txt']);
+        %                     end
+        %                     if isempty(fn)
+        %                      fn = dirc([ARGO_SYS_PARAM.iridium_path '*.' pn '.*science_log.csv']);
+        %                     end
+        %                     if isempty(fn)
+        %                      fn = dirc([ARGO_SYS_PARAM.iridium_path '*.' pn '.*system_log.txt']);
+        %                     end
+        %                     if isempty(fn)
+        %                        disp('file not found')
+        % %                         continue
+        %                     end
+        %                     for bb = 1:size(fn,1)
+        %                         pmeta.ftp_fname{bb} = fn{bb,1};
+        %                         pmeta.ftptime(bb) = julian(datevec(fn{bb,4}));
+        %                     end
+        %
+        
+        opts.rtmode = 0; %don't send BUFR files etc
+        opts.redo = 1;
+        
+        %                     try
+        
+        % When running process_iridium_apf11, comment out lines
+        % 842 to 850 (the bit that writes reprocessing back to file)
+        % You can also comment out the plotting scripts:
+        % web_float_summary; time_section_plot; waterfallplots; locationplots; tsplots;
+        
+        %                      process_iridium_apf11(pmeta,dbdat,opts) % FOR APF11s
+        
+        
+        process_iridium(pmeta,dbdat) % FOR APF9s and NAVIS            %%%%% <<-----
+        
+        %                     catch
+        %                         continue
+    end
+    
+end
 
-                    if isempty(fn)
-                       disp('file not found')
-                        continue
-                    end
-                    
-%                     for bb = 1:size(fn,1)
-%                         pmeta.ftp_fname{bb} = fn{bb,1};
-%                         pmeta.ftptime(bb) = julian(datevec(fn{bb,4}));
-%                     end
-                    opts.rtmode = 0; %don't send BUFR files etc
-                    opts.redo = 1;
-                    try
-%                         process_iridium_apf11(pmeta,dbdat,opts) 
-%                      process_iridium(pmeta,dbdat) 
-                     
-                    catch
-                        continue
-                    end
-                      argoprofile_nc(dbdat,fpp(j)) 
-                     
+%%
+% If you run process_iridium, no need to run argoprofile_nc
+fpp(j).p_calibrate = []; % Make p_calibrate empty to re-do qc_tests
+argoprofile_nc(dbdat,fpp(j))  %%%%% <<-----
+%copy to export
+system(['cp ' ARGO_SYS_PARAM.root_dir 'netcdf/' num2str(dbdat.wmo_id) '/R' num2str(dbdat.wmo_id) '_' pn '.nc /home/argo/ArgoRT/export'])
+
+
 %                     argoprofile_Bfile_nc(dbdat,fpp(j))
-                    %copy to export
-% system(['cp ' ARGO_SYS_PARAM.root_dir '/netcdf/' num2str(dbdat.wmo_id) '/BR' num2str(dbdat.wmo_id) '_' pn '.nc /home/argo/ArgoRT/export'])
-                    % or could run argoprofile_nc here too
+%copy to export
+%                     system(['cp ' ARGO_SYS_PARAM.root_dir '/netcdf/' num2str(dbdat.wmo_id) '/BR' num2str(dbdat.wmo_id) '_' pn '.nc /home/argo/ArgoRT/export'])
+% or could run argoprofile_nc here too
 %                 catch
 %                     bad = [bad;ii,j];
 %                 end
-            end
+% end % end loop in profile <<<<<<<<<------
 %         end
 %     end
-end
+% end % end loop in floats <<<<<<<<<,-------
