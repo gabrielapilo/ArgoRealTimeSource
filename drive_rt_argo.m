d142454f466dba57b6d38e3189b59c444ab9294c
Semolini Pilo, Gabriela (O&A, Hobart) <Gabriela.Semolinipilo@csiro.au>
2023-01-09 09:10:16 +1100

    Muted argos processing again

--- drive_rt_argo.m
+++ drive_rt_argo.m
@@ -6,24 +6,24 @@ dbclear if warning
 
 % addpath src
 
-[st,lfile] = system('ls -1tr argos_downloads_new/argos2*.log | tail -1');
-if st~=0
-  error('Cannot find latest download file');
-end
+%[st,lfile] = system('ls -1tr argos_downloads_new/argos2*.log | tail -1');
+%if st~=0
+%  error('Cannot find latest download file');
+%end
 
 global ARGO_SYS_PARAM
 
 set_argo_sys_params
 
 % Check to see if there are any argos files available
-today = now;
-day1 = datenum(str2num(datestr(now,'YYYY')),1,1);
-doy = round(today-day1);
+%today = now;
+%day1 = datenum(str2num(datestr(now,'YYYY')),1,1);
+%doy = round(today-day1);
 
-if doy - str2num(lfile(end-7:end-5)) >= 1; % if argos files too old, process iridium only
+%if doy - str2num(lfile(end-7:end-5)) >= 1; % if argos files too old, process iridium only
     strip_argos_msg('iridium')
-else
-   strip_argos_msg(lfile)
-end
+%else
+%   strip_argos_msg(lfile)
+%end
 
 web_processing_report
